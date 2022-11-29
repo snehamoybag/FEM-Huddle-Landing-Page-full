d42847eace3f745a2ea2a8a699fd24615425308f
snehamoybag <snehamoybag2@gmail.com>
2022-11-29 13:56:08 +0530

    do tablet layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,31 @@
 // Stylesheet
 import '/scss/style.scss'
+
+// javascript
+'use strict';
+
+const newsletterForm = document.querySelector('#newsletter-form');
+const newsletterEmail = document.querySelector('#newsletter-email');
+const emailRegex = /(^\S{1,64})@(\S*[a-zA-Z0-9]{1,255}$)/;
+
+// add error msg when invalid
+newsletterEmail.addEventListener('invalid', () => {
+  newsletterForm.classList.add('error');
+});
+
+// remove error when user types
+newsletterEmail.addEventListener('input', () => {
+  newsletterForm.classList.remove('error');
+});
+
+// add error msg  on submit when email is invalid
+newsletterForm.addEventListener('submit', e => {
+  if (!emailRegex.test(newsletterEmail.value)) {
+    e.preventDefault();
+    newsletterForm.classList.add('error');
+  }
+  else {
+    newsletterForm.classList.remove('error');
+    alert('Thankyou for subscribing to our weekly newsletters!');
+  }
+});
